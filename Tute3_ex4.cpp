e231f8b5a51037e0eb1e2a025f33f7e4315f0761
IT22228444 <127375039+IT22228444@users.noreply.github.com>
2023-03-23 06:12:24 +0000

    Tutorial 3

---
+++ Tute3_ex4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void print(int len, int wth);
+void input(int &len, int &wth);
+
+int main()
+{
+   int length = 10, width = 5;
+   input(length, width);
+   print(length, width);
+   return 0;
+}
+
+void print(int len, int wth) {
+ cout << "Length : " << len
+ << ", Width : " << wth << endl;
+}
+void input(int &len, int &wth)
+{
+  cout<<"Input length :";
+  cin>>len;
+  cout<<"Input width :";
+  cin>>wth;
+}
--- main.cpp
+++ main.cpp
@@ -61,3 +61,68 @@ int volume(int height, int width, int length)
   return volume;
 }*/
 
+/*#include <iostream>
+using namespace std;
+struct box
+{
+  int height;
+  int width;
+  int length;
+}box1,box2;
+int volume(int height, int width, int length);
+int main()
+{ 
+  
+   int totalVolume;
+
+  cout << "Enter Box 1 Height : ";
+ cin >> box1.height;
+ cout << "Enter Box 1 Width : ";
+ cin >> box1.width;
+ cout << "Enter Box 1 Length : ";
+  cin >> box1.length;
+cout<<endl;
+  
+  cout << "Enter Box 2 Height : ";
+ cin >> box2.height;
+ cout << "Enter Box 2 Width : ";
+ cin >> box2.width;
+ cout << "Enter Box 1 Length : ";
+  cin >> box2.length;
+cout<<endl;
+  totalVolume = volume(box1.height ,box1.width , box1.length)
+ + volume( box2.height, box2.width,box2.length );
+
+  cout << "Volume of Box is " << totalVolume << endl;
+
+ return 0;
+}
+int volume(int height, int width, int length)
+{
+  return height*width*length;
+}*/
+
+/*#include <iostream>
+using namespace std;
+void print(int len, int wth);
+void input(int &len, int &wth);
+
+int main()
+{
+   int length = 10, width = 5;
+   input(length, width);
+   print(length, width);
+   return 0;
+}
+
+void print(int len, int wth) {
+ cout << "Length : " << len
+ << ", Width : " << wth << endl;
+}
+void input(int &len, int &wth)
+{
+  cout<<"Input length :";
+  cin>>len;
+  cout<<"Input width :";
+  cin>>wth;
+}*/
